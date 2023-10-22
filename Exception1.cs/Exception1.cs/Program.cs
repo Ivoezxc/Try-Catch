2e522876a21a5273d89e70ec986a7c909321197c
John Ivoe Labasan <144910568+Ivoezxc@users.noreply.github.com>
2023-10-23 00:12:43 +0800

    Add files via upload

---
+++ Exception1.cs/Exception1.cs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter a non-negative integer: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number) && number >= 0)
+            {
+                double squareRoot = Math.Sqrt(number);
+                Console.WriteLine($"The square root of {number} is: {squareRoot:F2}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a non-negative integer.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
